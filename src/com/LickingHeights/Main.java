ea6ff94d758a08686d224f9fa0e0546b8bcf1b28
Jason Bordeau <jason.bordeau.2021@student.lhschools.org>
2020-03-05 12:10:59 -0500

    UserDataCaptured

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,24 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+       ModulusClock();
     }
+
+    public static void ModulusClock(){
+        int hours;
+        int minutes;
+        Scanner user;
+        user = new Scanner(System.in);
+
+        System.out.println("Enter a Value for Hours");
+        hours = user.nextInt();
+
+        System.out.println("Enter a Value for Minutes");
+        minutes = user.nextInt();
+    }
+
 }
